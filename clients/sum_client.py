f8f37d439ade6537f73d339a0ec6d85a176a3e94
user <roma1off1ac@gmail.com>
2020-10-14 17:02:04 +0300

    fix client

--- clients/sum_client.py
+++ clients/sum_client.py
@@ -9,26 +9,19 @@ MAX_CLIENTS = 1000
 URL = 'http://0.0.0.0:5050/'
 
 
-async def send_request(session, pid):
-    payload = {
-        'num1': random.randint(0, 2000),
-        'num2': random.randint(0, 2000)
-    }
-    async with session.get(URL, params=payload) as response:
-        response.close()
-
-
-async def asynchronous_sending():
-    start = time.time()
-
+async def send_request():
     async with aiohttp.ClientSession(json_serialize=json.dumps) as session:
-        tasks = [asyncio.ensure_future(send_request(session, pid)) for pid in range(1, MAX_CLIENTS + 1)]
-        await asyncio.gather(*tasks)
-
-    print(f"Process took: {time.time() - start} seconds")
-
+        payload = {
+            'num1': random.randint(0, 2000),
+            'num2': random.randint(0, 2000)
+        }
+        async with session.get(URL, params=payload) as response:
+            return await response.text()
 
 if __name__ == '__main__':
     eloop = asyncio.get_event_loop()
-    eloop.run_until_complete(asynchronous_sending())
+    start = time.time()
+    tasks = [asyncio.ensure_future(send_request()) for pid in range(1, MAX_CLIENTS + 1)]
+    eloop.run_until_complete(asyncio.gather(*tasks))
+    print(f"Process took: {time.time() - start} seconds")
     eloop.close()
